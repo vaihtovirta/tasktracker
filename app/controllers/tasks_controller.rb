9c2c03f0b2c549c9a0b37831b74c8bac4a87264a
Emil Shakirov <5o.smoker@gmail.com>
2014-07-08 20:15:53 +0000

    Added state difference and Edit\Delete buttons

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,10 +10,28 @@ class TasksController < ApplicationController
   
   def index
     @opened_tasks = Task.where(state: 0)
+    @work_tasks = Task.where(state: 1)
+    @qa_tasks = Task.where(state: 2)
+    @approved_tasks = Task.where(state: 3)
+    @dropped_tasks = Task.where(state: 4)
+  end
+ 
+  def edit
+    @task = Task.find(params[:id])
+    @task_collection = Task.state_machine.states.map { |state| [state.name, state.value]}
   end
   
+  def update 
+    Task.find(params[:id]).update_attributes(task_params)
+    redirect_to action: :index 
+  end
+  
+  def destroy 
+    Task.find(params[:id]).delete
+    redirect_to action: :index 
+  end
   private
   def task_params 
-    params.require(:task).permit(:title, :description, :rating)
+    params.require(:task).permit(:title, :description, :rating, :state)
   end
 end
